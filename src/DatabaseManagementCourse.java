9b951751ab68a598aa53b9244cb3429e0715572c
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-10-22 18:16:15 +0500

    Update DatabaseManagementCourse.java

--- src/DatabaseManagementCourse.java
+++ src/DatabaseManagementCourse.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class DatabaseManagementCourse {
+public class DatabaseManagementCourse implements Course {
+    @Override
+    public void deliverContent() {
+    }
 }
