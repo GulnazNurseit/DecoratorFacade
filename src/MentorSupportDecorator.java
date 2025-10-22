6dcff77ccd76ececad99ec880485607a25ee453f
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-10-22 18:10:05 +0500

    Refactor MentorSupportDecorator to extend EnhancedCourseDecorator

--- src/MentorSupportDecorator.java
+++ src/MentorSupportDecorator.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public class MentorSupportDecorator extends EnhancedCourseDecorator {
 
-public class MentorSupportDecorator {
+    public MentorSupportDecorator(Course course) {
+        super(course);
+    }
+
+    @Override
+    public void deliverContent() {
+        course.deliverContent();
+        System.out.println("Mentor support added.");
+    }
+
+    @Override
+    public void updateProgress(int newProgress) {
+        // Update progress in the mentor support decorator
+        this.progress = newProgress;
+        System.out.println("Progress updated with mentor support: " + progress + "%");
+    }
 }
