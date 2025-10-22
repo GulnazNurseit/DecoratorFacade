88763a40497ca45f8f1bdb09df4ecbec1eef1eff
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-10-22 18:15:32 +0500

    Refactor CertificateDecorator to extend EnhancedCourseDecorator
    Refactor CertificateDecorator to extend EnhancedCourseDecorator and implement content delivery and progress update.

--- src/CertificateDecorator.java
+++ src/CertificateDecorator.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public class CertificateDecorator extends EnhancedCourseDecorator {
 
-public class CertificateDecorator {
+    public CertificateDecorator(Course course) {
+        super(course);
+    }
+
+    @Override
+    public void deliverContent() {
+        course.deliverContent();
+        System.out.println("Certificate added upon course completion.");
+    }
+
+    @Override
+    public void updateProgress(int newProgress) {
+        // Implementation for updating progress in the certificate decorator
+        this.progress = newProgress;
+        System.out.println("Progress updated for certificate: " + progress + "%");
+    }
 }
