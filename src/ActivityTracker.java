530bbe0184512200da3a2b3560fd27180f856a55
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-10-22 18:15:02 +0500

    Implement activity logging and display methods
    Added methods to log and display student activities.

--- src/ActivityTracker.java
+++ src/ActivityTracker.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityTracker {
+    private List<String> activities;
+
+    public ActivityTracker() {
+        this.activities = new ArrayList<>();
+    }
+
+    // Log activity of the student
+    public void logActivity(String activity) {
+        activities.add(activity);
+        System.out.println("Activity logged: " + activity);
+    }
+
+    // Display all activities
+    public void displayActivities() {
+        System.out.println("All activities of the student:");
+        for (String activity : activities) {
+            System.out.println(activity);
+        }
+    }
 }
