27db208075fffc360d1bcc70e71a752f5ea2901e
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-10-24 11:20:30 +0500

    Implement Course interface in WebTechnologiesCourse

--- src/WebTechnologiesCourse.java
+++ src/WebTechnologiesCourse.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class WebTechnologiesCourse {
+// Web Technologies Course
+public class WebTechnologiesCourse implements Course {
+    @Override
+    public void deliverContent() {
+    }
 }
